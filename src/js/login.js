1f650298a055f53d1c7356b31321537226039f02
douglasmariano <douglas.arm@live.com>
2023-09-28 17:37:43 -0300

    corrigindo bugs parte 4

--- src/js/login.js
+++ src/js/login.js
@@ -5,6 +5,17 @@ function login() {
   // Recuperar os usuários do Local Storage
   let usuarios = JSON.parse(localStorage.getItem("usuarios") || "[]");
 
+  // Verificar se não existem usuários
+  if (usuarios.length === 0) {
+    Swal.fire({
+      icon: "info",
+      title: "Oops...",
+      text: "Não há usuários cadastrados!",
+      footer: "Cadastre-se primeiro"
+    });
+    return;
+  }
+
   for (const user of usuarios) {
     if (user.nome === name && user.senha === password) {
       window.location.href = "./src/html/buscaEndereco.html";
